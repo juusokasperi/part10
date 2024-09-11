9ac02494068b2d2f685ec1ed6ed85394a48f55eb
juusokasperi <juusor@gmail.com>
2024-09-11 14:12:34 +0300

    ex10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,17 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+	const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+		fetchPolicy: 'cache-and-network',
+	});
+
+	return { repositories: data?.repositories,
+			error,
+			loading,
+			refetch: () => { return refetch(); }
+		};
+};
+
+export default useRepositories;
